9b2ef77faf5dd0efa3fd0fed974202c86d01bcbf
Lucas Silva <lucasiilva03@gmail.com>
2023-01-05 12:00:54 +0000

    fix js bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import RegisterView from '../views/RegisterView.vue'
 import LandingPage from '../views/LandingPage.vue'
+import LandingPageView from '../views/LandingPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,8 +20,8 @@ const router = createRouter({
       component: RegisterView
     },
     {
-      path: '/landingPage',
-      name: 'landingPage',
+      path: '/LandingPageView',
+      name: 'LandingPageView',
       component: LandingPageView
     },
   ]
